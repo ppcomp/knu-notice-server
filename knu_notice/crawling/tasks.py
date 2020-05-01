68fe3229ad7a34509a11def4ea4871c8f9085cf2
qwlake <qwlake@gmail.com>
2020-05-01 13:20:37 +0900

    feat: docker 개발 환경 세팅

---
+++ knu_notice/crawling/tasks.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+import logging
+from celery import Celery
+from celery.schedules import crontab
+
+from django.conf import settings
+from knu_notice.celery import app
+
+logger = logging.getLogger("celery")
+
+
+@app.task
+def show_hello_world():
+    logger.info("-"*25)
+    logger.info("Printing Hello from Celery")
+    logger.info("-"*25)
