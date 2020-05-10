bd10c58e47cbf6cab5b08906da8ec1aaa4962082
qwlake <qwlake@gmail.com>
2020-05-10 23:41:52 +0900

    fix: celery로 인한 scrapy 재실행시 나오는 에러 수정
    twisted.internet.error.ReactorNotRestartable 수정

--- knu_notice/crawling/tasks.py
+++ knu_notice/crawling/tasks.py
@@ -9,6 +9,7 @@ from knu_notice.celery import app
 import scrapy
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
+from scrapy.crawler import CrawlerProcess
 from scrapy.utils.log import configure_logging
 from .crawler.crawler.spiders.crawl_spider import CrawlSpiderSpider
 from scrapy.utils.project import get_project_settings
@@ -16,10 +17,6 @@ logger = logging.getLogger("celery")
 
 @app.task
 def crawling():
-    configure_logging()
-    runner = CrawlerRunner()
-    runner.crawl(CrawlSpiderSpider)
-    d = runner.join()
-    d.addBoth(lambda _: reactor.stop())
-
-    reactor.run()
+    process = CrawlerProcess()
+    process.crawl(CrawlSpiderSpider)
+    process.start(stop_after_crawl=False)
