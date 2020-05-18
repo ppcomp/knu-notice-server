bbe36a577e753a1936c307220c874129c464fd79
qwlake <qwlake@gmail.com>
2020-05-18 12:30:08 +0900

    feat: main 게시판 크롤링 기능 추가

--- knu_notice/crawling/tasks.py
+++ knu_notice/crawling/tasks.py
@@ -12,17 +12,24 @@ from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.log import configure_logging
-from .crawler.crawler.spiders.crawl_spider import CrawlSpiderSpider
+from .crawler.crawler.spiders import crawl_spider
 from scrapy.settings import Settings
 
 settings = Settings()
 os.environ['SCRAPY_SETTINGS_MODULE'] = 'crawling.crawler.crawler.settings'
 settings_module_path = os.environ['SCRAPY_SETTINGS_MODULE']
 settings.setmodule(settings_module_path, priority='project')
+spiders = [
+    # crawl_spider에 게시판 크롤링 class 생성 후 이 곳에 추가.
+    # 이 곳에 있는 게시판(class)을 대상으로 crawling됨.
+    crawl_spider.MainSpider,
+    crawl_spider.CseSpider,
+]
 
 def crawling_start():
     process = CrawlerProcess(settings)
-    process.crawl(CrawlSpiderSpider)
+    for spider in spiders:
+        process.crawl(spider)
     process.start()
 
 @app.task
