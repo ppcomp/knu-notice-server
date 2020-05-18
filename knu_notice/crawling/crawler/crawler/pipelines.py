f39cc61a1531daf4065cf071c06ab32e4b147822
jeongjun Hawng <hsk2454@naver.com>
2020-05-18 23:15:32 +0900

    feat : 새로 삽입되는 데이터 비교 추가
    crawler 폴더 내에 pipeline.py에서 기존 DB에 있는 데이터인지 새로운 데이터인지 비교하는 코드 추가

--- knu_notice/crawling/crawler/crawler/pipelines.py
+++ knu_notice/crawling/crawler/crawler/pipelines.py
@@ -7,13 +7,15 @@
 
 class CrawlerPipeline:
     def process_item(self, item, spider):
-        model = item['model'](
-            bid = item['bid'],
-            title = item['title'],
-            link = item['link'],
-            date = item['date'],
-            author = item['author'],
-            reference = item['reference'],
-        )
-        model.save()
+        #get_or_create : 매개변수로 들어오는 데이터를 DB에 있는 데이터와 비교해서 데이터를 Get 하거나 Create 함
+        notice, flag = item['model'].objects.get_or_create( bid = item['bid'],
+                                                    title = item['title'],
+                                                    link = item['link'],
+                                                    date = item['date'],
+                                                    author = item['author'],
+                                                    reference = item['reference'],)
+        #flag = True : DB에 저장된 같은 데이터가 없음 (Create)
+        #flag = False : DB에 저장된 같은 데이터가 있음 (Get)
+        if flag:
+            print("new Data insert!")
         return item
