33f35e82d4fd8bf3a39fc50a00d139222175c57d
qwlake <qwlake@gmail.com>
2020-05-19 15:55:20 +0900

    fix: get_or_create 기존 데이터 비교할 때 bid만 사용토록 변경

--- knu_notice/crawling/crawler/crawler/pipelines.py
+++ knu_notice/crawling/crawler/crawler/pipelines.py
@@ -8,14 +8,18 @@
 class CrawlerPipeline:
     def process_item(self, item, spider):
         #get_or_create : 매개변수로 들어오는 데이터를 DB에 있는 데이터와 비교해서 데이터를 Get 하거나 Create 함
-        notice, flag = item['model'].objects.get_or_create( bid = item['bid'],
-                                                    title = item['title'],
-                                                    link = item['link'],
-                                                    date = item['date'],
-                                                    author = item['author'],
-                                                    reference = item['reference'],)
-        #flag = True : DB에 저장된 같은 데이터가 없음 (Create)
-        #flag = False : DB에 저장된 같은 데이터가 있음 (Get)
-        if flag:
+        notice, created = item['model'].objects.get_or_create(
+            bid = item['bid'],  # bid만 일치하면 기존에 있던 데이터라고 판단함.
+            defaults={
+                'title':item['title'],
+                'link':item['link'],
+                'date':item['date'],
+                'author':item['author'],
+                'reference':item['reference'],
+            }
+        )
+        #created = True : DB에 저장된 같은 데이터가 없음 (Create)
+        #created = False : DB에 저장된 같은 데이터가 있음 (Get)
+        if created:
             print("new Data insert!")
         return item
