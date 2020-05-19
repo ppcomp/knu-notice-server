6215e5b073a89c29accb489cef57ff1dc47a3bc9
qwlake <qwlake@gmail.com>
2020-05-19 15:54:18 +0900

    refactor: 코드 정리, 크롤링 데이터 전처리 함수 정리

--- knu_notice/crawling/crawler/crawler/spiders/crawl_spider.py
+++ knu_notice/crawling/crawler/crawler/spiders/crawl_spider.py
@@ -12,6 +12,8 @@ from scrapy.linkextractors import LinkExtractor
 
 class DefaultSpider(scrapy.Spider):
 
+    # 객체 인스턴스에서 사용되는 변수 등록
+    # params: model, bid, url_xpath, titles_xpath, dates_xpath, authores_xpath, references_xpath
     def set_args(self, model, bid, url_xpath, titles_xpath, dates_xpath, authors_xpath, references_xpath):
         self.model = model
         self.bid = bid
@@ -21,41 +23,64 @@ class DefaultSpider(scrapy.Spider):
         self.authors_xpath = authors_xpath
         self.references_xpath = references_xpath
 
-    def bid_split(self, url):
-        idx = url.find(self.bid)+len(self.bid)+1
-        for i in range(idx, len(url)):
-            if url[i] == "&":
-                return url[idx:i]
-        return url[idx:]
-
-    def remove_null(self, items):
+    # 공백 제거. 가장 선행되어야 하는 전처리
+    # params: items
+    # return: ret
+    def remove_whitespace(self, items):
         ret = []
-        x = ''
         for item in items:
             x = item.strip()
             if x != '':
                 ret.append(x)
         return ret
 
+    # Link 객체에서 url과 bid 추출
+    # params: links
+    # return: bids, urls
+    def split_bid_and_link(self, links):
+        urls = []
+        bids = []
+        for link in links:
+            urls.append(link.url+'&')
+        for url in urls:
+            idx = url.find(self.bid)+len(self.bid)+1
+            for i in range(idx, len(url)):
+                if url[i] == "&":
+                    break
+            bids.append(url[idx:i])
+        return bids, urls
+
+    # date 형식에 맞게 조정
+    # ex 2020.05.19 > 2020-05-19
+    # params: dates
+    # return: dates
+    def date_cleanse(self, dates):
+        return [date.replace('.','-') for date in dates]
+
     def parse(self, response):
-        url_form = LinkExtractor(restrict_xpaths=self.url_xpath,attrs='href')
-        urls = url_form.extract_links(response)
-        titles = self.remove_null(response.xpath(self.titles_xpath).extract())
-        dates = self.remove_null(response.xpath(self.dates_xpath).extract())
-        authors = self.remove_null(response.xpath(self.authors_xpath).extract())
+        url_forms = LinkExtractor(restrict_xpaths=self.url_xpath,attrs='href')
+        links = url_forms.extract_links(response)
+        titles = self.remove_whitespace(response.xpath(self.titles_xpath).extract())
+        dates = self.remove_whitespace(response.xpath(self.dates_xpath).extract())
+        authors = self.remove_whitespace(response.xpath(self.authors_xpath).extract())
         if self.references_xpath:
-            references = self.remove_null(response.xpath(self.references_xpath).extract())
+            references = self.remove_whitespace(response.xpath(self.references_xpath).extract())
         else:
-            references = [None for _ in range(len(urls))]
-        for item in zip(urls, titles, dates, authors, references):
+            references = [None for _ in range(len(links))]
+
+        # Data cleansing
+        bids, links = self.split_bid_and_link(links)    # bid, link 추출
+        dates = self.date_cleanse(dates)                # date 형식에 맞게 조정
+
+        for item in zip(bids, titles, links, dates, authors, references):
             scrapyed_info = {
                 'model' : self.model,
-                'bid' : self.bid_split(item[0].url),
+                'bid' : item[0],
                 'title' : item[1],
-                'link' : item[0].url,
-                'date' : item[2].replace('.','-'),
-                'author' : item[3],
-                'reference' : item[4] if item[4] else None, # reference가 있다면 strip()
+                'link' : item[2],
+                'date' : item[3],
+                'author' : item[4],
+                'reference' : item[5],
             }
             yield scrapyed_info
 
