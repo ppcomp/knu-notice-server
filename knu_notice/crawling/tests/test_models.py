91cdadc523a5977ebb00533352187011f5307056
jungwoo <qwlake@gmail.com>
2020-05-27 23:54:33 +0900

    feat: 기존 코드에 대한 test code 작성

--- knu_notice/crawling/tests/test_models.py
+++ knu_notice/crawling/tests/test_models.py
@@ -1,6 +1,25 @@
-import pytest
+# from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from model_mommy import mommy
 
-@pytest.mark.django_db
-def test_my_user():
-    me = User.objects.get(username='me')
-    assert me.is_superuser
+from crawling import models
+
+class TestPoint(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.model_list = []
+        for name, cls in models.__dict__.items():
+            if isinstance(cls, type):
+                self.model_list.append(mommy.make(cls))
+
+    def test_create_models(self):
+        for model in self.model_list:
+            assert model
+
+    def test_get_notice_all(self):
+        response = self.client.get('/notice/all/')
+        assert response.status_code==200
+
+    def test_get_notice_all_query(self):
+        response = self.client.get('/notice/all/?board=cse-main')
+        assert response.status_code==200
