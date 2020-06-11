d8ecfbc7c5b173f63ba166b8c03b799bba07423b
qwlake <qwlake@gmail.com>
2020-06-12 00:07:15 +0900

    fix: static files의 404 버그 수정

--- knu_notice/knu_notice/dev/settings.py
+++ knu_notice/knu_notice/dev/settings.py
@@ -24,7 +24,7 @@ def get_secret(setting):
         raise ImproperlyConfigured(error_msg)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
