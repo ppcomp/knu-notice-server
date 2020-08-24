b6e190e7a84a3c3627f799ce6c1468ab6d17dad4
qwlake <qwlake@gmail.com>
2020-08-25 03:14:50 +0900

    fix: device 등록 버그 수정

--- knu_notice/knu_notice/celery.py
+++ knu_notice/knu_notice/celery.py
@@ -14,7 +14,7 @@ app = Celery(
 app.conf.beat_schedule = {
     'crawling_task': {
         'task': 'crawling.celery_tasks.crawling_task.crawling_task',
-        'schedule': timedelta(seconds=300),
+        'schedule': crontab(minute=15), # timedelta(seconds=900),
         'args': (1,-1,True),
         'options': {'queue' : 'crawling_tasks'},
     }
